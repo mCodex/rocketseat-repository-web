106e0f7cdce6adba3e829358d73deb04c8258ac2
Mateus Andrade <mat.andrade@live.com>
2020-02-23 10:20:50 -0300

    Updates

---
+++ precache-manifest.939df8ddb4ada664bb0a3ac90be856ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25ee1e6f754477a4adb378d123fd9af",
+    "url": "/rocketseat-repository-web/index.html"
+  },
+  {
+    "revision": "52af588ebc5fa7f00f47",
+    "url": "/rocketseat-repository-web/static/js/2.576d69b5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/rocketseat-repository-web/static/js/2.576d69b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d127c72f38719f6bc86b",
+    "url": "/rocketseat-repository-web/static/js/main.d5f467e9.chunk.js"
+  },
+  {
+    "revision": "fa71a339152a4d4bef55",
+    "url": "/rocketseat-repository-web/static/js/runtime-main.0fd25bea.js"
+  }
+]);
