69b029415c660b62ef5602f88fa15e4491b18639
Mateus Andrade <mat.andrade@live.com>
2020-02-23 09:58:52 -0300

    added axios & styling main.js

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,54 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { Title } from './styles';
+import api from '../../services/api';
+
+import { Container, Form, SubmitButton } from './styles';
 
 const Main = () => {
-    return <Title>Hello World</Title>;
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleInputChange = e => setNewRepo(e.target.value);
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setLoading(true);
+        const response = await api.get(`/repos/${newRepo}`);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        setRepositories(prevState => [...prevState, data]);
+        setNewRepo('');
+        return setLoading(false);
+    };
+
+    return (
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositórios
+            </h1>
+            <Form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Adicionar Repositório"
+                    value={newRepo}
+                    onChange={handleInputChange}
+                />
+                <SubmitButton loading={loading}>
+                    {loading ? (
+                        <FaSpinner color="#fff" size={14} />
+                    ) : (
+                        <FaPlus color="#fff" size={14} />
+                    )}
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 };
 
 export default memo(Main);
